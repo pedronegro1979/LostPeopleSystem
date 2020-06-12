c9ab3539738e9b344e6558116743b5cc6a465da2
pedronegro1979 <45272527+pedronegro1979@users.noreply.github.com>
2020-06-12 07:23:33 +0200

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LostPeopleSystem
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "LostPeopleSystem", action = "Index", id = UrlParameter.Optional },
+                 namespaces: new[] { "RecreationalServicesTicketingSystem.Controllers" }
+            );
+
+            routes.MapRoute(
+                name: "LostPeopleSystem",
+                url: "{controller}/{action}/{name}/{id}"
+            );
+        }
+    }
+}
